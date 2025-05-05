a0f04487f9f3382cf3eea2e673d820cf00ec4a40
John 'Snow' Close <SnowdramaGames@gmail.com>
2025-05-05 01:23:49 -0400

    Remove Multiplayer from main tools

--- Multiplayer/MultiplayerGlobals.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-using System;
-
-public partial class MultiplayerGlobals
-{
-    public static bool ShowDebug = true;
-    public static string GameFilterKey = "Snowdrama";
-    public static uint SteamAppId = 480;
-}
